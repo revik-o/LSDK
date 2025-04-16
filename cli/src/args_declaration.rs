ba06d9ac2b1ec87009d423de8fdc1d7a07571bc2
revik-o <oleg.03.07.2000@gmail.com>
2025-04-16 15:10:48 +0300

    init: upd CLI

---
+++ cli/src/args_declaration.rs
@@ -0,0 +1,48 @@
+#[derive(Clone, Copy)]
+pub enum CommandToken {
+    New = 0,
+    Help = 1,
+    Type = 2,
+    List = 3,
+    Name = 4,
+    Project = 5,
+    Module = 6,
+    AutoComplete = 7,
+}
+
+#[derive(Clone)]
+pub struct ArgsDeclaration {
+    pub command: CommandToken,
+    pub args: Vec<String>,
+}
+
+pub struct CommandDeclarationPosition {
+    pub command: CommandToken,
+    pub position: usize,
+}
+
+impl CommandDeclarationPosition {
+    pub fn new(command: CommandToken, position: usize) -> Self {
+        CommandDeclarationPosition { command, position }
+    }
+}
+
+impl ArgsDeclaration {
+    pub fn new(command: CommandToken, args: Vec<&str>) -> Self {
+        let args = args.into_iter().map(|s| s.to_string()).collect();
+        ArgsDeclaration { command, args }
+    }
+}
+
+pub fn supported_commands() -> [ArgsDeclaration; 8] {
+    [
+        ArgsDeclaration::new(CommandToken::New, vec!["new"]),
+        ArgsDeclaration::new(CommandToken::Help, vec!["help", "h", "-?", "--help", "-h"]),
+        ArgsDeclaration::new(CommandToken::Type, vec!["type", "t", "--type", "-t"]),
+        ArgsDeclaration::new(CommandToken::List, vec!["list", "l", "ls", "--list", "-l"]),
+        ArgsDeclaration::new(CommandToken::Name, vec!["name", "n", "--name", "-n"]),
+        ArgsDeclaration::new(CommandToken::Project, vec!["project", "p"]),
+        ArgsDeclaration::new(CommandToken::Module, vec!["module", "m"]),
+        ArgsDeclaration::new(CommandToken::AutoComplete, vec!["auto-complete"]),
+    ]
+}
---
+++ cli/src/utils/args.rs
@@ -0,0 +1,46 @@
+use std::env::{self, Args};
+
+use crate::args_declaration::{
+    ArgsDeclaration, CommandDeclarationPosition, CommandToken, supported_commands,
+};
+
+fn recognize_command(dec_ptr: &ArgsDeclaration, arg_val_ptr: &String) -> bool {
+    let args = dec_ptr.clone().args;
+    args.into_iter().any(|val| val == (*arg_val_ptr))
+}
+
+pub fn parce_args_to_tokens() -> Vec<CommandDeclarationPosition> {
+    let mut result: Vec<CommandDeclarationPosition> = vec![];
+    let supported_commands = supported_commands();
+
+    for (index, value) in env::args().enumerate() {
+        let command = supported_commands
+            .iter()
+            .find(|ptr| recognize_command(*ptr, &value));
+
+        match command {
+            None => continue,
+            Some(ptr) => {
+                let declaration = ptr.clone();
+                result.push(CommandDeclarationPosition::new(declaration.command, index));
+            }
+        }
+    }
+
+    result
+}
+
+pub fn get_command_value(position: CommandDeclarationPosition) -> Option<String> {
+    match position.command {
+        CommandToken::Name | CommandToken::Type => {
+            let args: Vec<String> = env::args().collect();
+
+            if args.len() > position.position + 1 {
+                return Option::Some(args[position.position + 1].clone());
+            }
+
+            Option::None
+        }
+        _ => Option::None,
+    }
+}
